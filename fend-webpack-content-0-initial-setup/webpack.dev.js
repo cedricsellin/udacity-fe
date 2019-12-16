36e7fca3260237e1864bdfae6e3bdf8296c0b708
Cedric Sellin <csellin@gmail.com>
2019-12-16 22:52:48 +0100

    Initial checkin

--- fend-webpack-content-0-initial-setup/webpack.dev.js
+++ fend-webpack-content-0-initial-setup/webpack.dev.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const webpack = require('webpack')
+const CleanWebpackPlugin  = require('clean-webpack-plugin').CleanWebpackPlugin
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
+    mode: 'development',
     entry: './src/client/index.js',
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
-                loader:['babel-loader','eslint-loader']
+                loader: ['babel-loader', 'eslint-loader']
             }
         ]
     },
@@ -17,6 +20,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html"
-        })
+        }),
+        new CleanWebpackPlugin(),
+        new BundleAnalyzerPlugin()
+        
     ]
 }
