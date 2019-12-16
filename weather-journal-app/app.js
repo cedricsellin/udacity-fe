ea39b3f41b6b8d3a232c8b65ef059d5c77b0353c
Cedric Sellin <csellin@gmail.com>
2019-12-16 10:19:01 +0100

    fixing server and client side issues

--- weather-journal-app/app.js
+++ weather-journal-app/app.js
@@ -1,4 +1,5 @@
 const maxFeelingsLength = 2000
+let errorField
 
 function convertJSONtoHTML(json) {
     let html = `<div class="entrydate">${json.date}</div> 
@@ -8,8 +9,66 @@ function convertJSONtoHTML(json) {
     return html
 }
 
-function clickOnGenerate() {
-    const errorField = document.getElementById('error')
+const postData = async (url, data) => {
+    let success = true
+    //TODO: CHECK WE DEAL WITH SERVER ERRORS
+    //TODO: HOW TO PROPAGATE THE ERRORS
+    try {
+        let resp = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header        
+        })
+
+        if (resp.status != 200) {
+            errorField.innerHTML = 'Server Error '+resp.status
+            success = false
+        }
+    } catch (error) {
+        success = false
+        // Main error is the ZipCode Lookup
+        errorField.innerHTML = 'Error uploading the data to the server'
+    } 
+
+    return success
+    
+}
+
+const getWeatherdata = async (zipcode) => {
+    const APIKEY = "727fe42fb2c8c85e8121c5bdbfaf4804"
+    const weatherURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${APIKEY}`
+
+    const query = fetch(weatherURL)
+        .then(response => response.json())
+        .then(info => info.main.temp)
+        .catch(err => {
+            errorField.innerHTML = err.toString()
+        });
+
+    return query
+}
+
+
+//This function will return TRUE or FALSE based on the success of the data push...
+async function pushDataToServer(temp) {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+    const feelingsContent = document.getElementById('feelings').value
+
+    const fields = { date: `${yyyy}-${mm}-${dd}`, temp: temp, content: `${feelingsContent}` }
+
+    let success = postData('http://localhost:8080/newentry', fields)
+
+    return success
+}
+
+
+async function clickOnGenerate() {
     let errorString = null
     let fieldToFocus = null
     const zipCode = document.getElementById('zip').value
@@ -31,40 +90,21 @@ function clickOnGenerate() {
         return
     }
 
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    const yyyy = today.getFullYear();
+    //TODO: FIX
+    try {
+        const temp = await getWeatherdata(zipCode)
+        const upload = await pushDataToServer(convertKelvinToCelsius(temp))
+        if (upload)
+            updateEntries()
 
-    const fields = { date: `${yyyy}-${mm}-${dd}`, zip: zipCode, content: `${feelingsContent}` }
-
-    const postData = async (url, data) => {
-
-        const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data), // body data type must match "Content-Type" header        
-        })
-
-        try {
-            const newData = await response.json();
-            console.log(newData);
-            updateMostRecentEntries(newData)
-            return newData
-        } catch (error) {
-            console.log("error in clickOnGenerate", error);
-        }
+        //Resetting the fields after the entry is logged
+        document.getElementById('zip').value = ""
+        document.getElementById('feelings').value = ""
+    }
+    catch (error) {
+        errorField.innerHTML=error.toString()
     }
 
-    postData('http://localhost:8080/newentry', fields)
-
-    //Resetting the fields after the entry is logged
-    document.getElementById('zip').value=""
-    document.getElementById('feelings').value=""
-    return;
 }
 
 function validateContent(value) {
@@ -77,6 +117,10 @@ function validateContent(value) {
 
 }
 
+function convertKelvinToCelsius(kelvin) {
+    return (Math.round(kelvin - 273.15))
+}
+
 function validateZipCode(value) {
     return /^\d{5}(-\d{4})?$/.test(value);
 }
@@ -85,8 +129,14 @@ function validateZipCode(value) {
 document.addEventListener('DOMContentLoaded', function () {
     // Now that the DOM is loaded we can listen for a click on the button
     document.getElementById('generate').addEventListener('click', clickOnGenerate)
-    initializeRecentEntries()
-    //Load the content from the server for the previous data
+    errorField = document.getElementById('error')
+
+    try {
+        //Load the content from the server for the previous data
+        updateEntries()
+    } catch (error) {
+        errorField.innerHTML = "There was an error with the server getting the data"
+    }
 })
 
 function updateMostRecentEntries(data) {
@@ -102,23 +152,14 @@ function updateMostRecentEntries(data) {
 
 
 //Needs to become an Async function...
-function initializeRecentEntries() {
+function updateEntries() {
 
-    const fetchNewData = async (url) => {
-        const response = await fetch(url, { credentials: 'same-origin' })
+    fetch('http://localhost:8080/all', { credentials: 'same-origin' })
+        .then(response => response.json())
+        .then(data => updateMostRecentEntries(data))
+        .catch(err => {
+            errorField.innerHTML = err.toString()
+        });
 
-        try {
-            const data = await response.json();
-            console.log(data);
 
-            updateMostRecentEntries(data);
-
-            return data;
-        } catch (error) {
-            console.log("error in fetchNewData", error);
-        }
-
-    }
-
-    fetchNewData('http://localhost:8080/all');
 }
--- weather-journal-app/website/server.js
+++ weather-journal-app/website/server.js
@@ -3,14 +3,12 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
 
-const APIKEY = "727fe42fb2c8c85e8121c5bdbfaf4804"
 const portNum = 8080
 // Start up an instance of app
 /* Dependencies */
 const app = express()
-const data = [{ date: "date", temp: "temperature", content: "content" }]
+const data = []
 
 /* Middleware*/
 // Cors for cross origin allowance
@@ -26,34 +24,16 @@ app.use(bodyParser.json())
 // Initialize the main project folder
 // Callback function to complete GET '/all'
 app.get('/all', (req, resp) => {
-    console.log("got a GET request")
     resp.send(JSON.stringify(data))
 })
 
 // Post Route
 app.post('/newentry', (req, resp) => {
-
-    console.log(req.body)
-
-    const zipcode = req.body.zip
-
-    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${APIKEY}`
-
-    const projectData = fetch(url)
-        .then(response => response.json())
-        .then(info => info.main.temp)
-        .then(temp => {
-            data.push({ date: req.body.date, temp: convertKelvinToCelsius(temp), content: req.body.content })
-            console.log("TESTING ON THE /newentry with a get", data)
-            resp.send(JSON.stringify(data))
-        })
-        .catch(err => console.log('error' + err));
-
+    data.push(req.body)
+    resp.status(200)
+    resp.statusText = "Data Stored Successfully"
+    resp.send()
 })
-
-function convertKelvinToCelsius(kelvin) {
-    return (Math.round(kelvin - 273.15))
-}
 
 app.listen(portNum, () => {
     console.log(`Server Ready listening on port ${portNum}`)
